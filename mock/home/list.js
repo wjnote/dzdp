e670dd81bd69afb4d0037a96243017a89baa6b2f
wjnote <641471534@qq.com>
2017-10-19 20:42:22 +0800

    模拟首页用户的数据

---
+++ mock/home/list.js
@@ -0,0 +1,45 @@
+module.exports = {
+    hasMore: true,
+    data: [
+        {
+            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            title: '汉堡大大',
+            subTitle: '叫我汉堡大大，还你多彩口味',
+            price: '28',
+            distance: '120m',
+            mumber: '389'
+        },
+        {
+            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            title: '北京开源饭店',
+            subTitle: '[望京]自助晚餐',
+            price: '98',
+            distance: '140m',
+            mumber: '689'
+        },
+        {
+            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            title: '服装定制',
+            subTitle: '原价xx元，现价xx元，可修改一次',
+            price: '1980',
+            distance: '160',
+            mumber: '106'
+        },
+        {
+            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            title: '婚纱摄影',
+            subTitle: '免费试穿，拍照留念',
+            price: '2899',
+            distance: '160',
+            mumber: '58'
+        },
+        {
+            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            title: '麻辣串串烧',
+            subTitle: '双人免费套餐等你抢购',
+            price: '0',
+            distance: '160',
+            mumber: '1426'
+        }
+    ]
+}
