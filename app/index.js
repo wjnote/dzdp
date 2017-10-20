b018f655fd7b452fd31cbb8e66ea3f33bf7c7aca
wjnote <641471534@qq.com>
2017-10-20 17:03:40 +0800

    引入了redux状态管理

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { browserHistory } from 'react-router';
 import RouteMap from './router/routerMap';
 import configureStore from './store/configureStore';
 import './util/htmlFontSize';
@@ -11,19 +10,11 @@ import './static/css/iconfont.css';
 import './static/css/common.less';
 
 
-
 // redux 第二步，生成store
 const store = configureStore();
-
 ReactDOM.render(
-	<Provider store={store}>
-		<RouteMap history={ browserHistory }/>
-	</Provider>,
-	document.getElementById('app')
+  <Provider store = { store } > 
+			<RouteMap / > 
+		</Provider>,
+  document.getElementById('app')
 );
-
-
-// mock 数据的
-// import { getData, postData } from './fetch/data';
-// getData();
-// postData();
