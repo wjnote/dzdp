d5a84c25cf4bd81355fea4c64eeb8ef46d2b212b
wjnote <641471534@qq.com>
2017-10-20 16:50:13 +0800

    修改模拟数据，让滑动加载更多效果明显

--- mock/home/list.js
+++ mock/home/list.js
@@ -2,7 +2,7 @@ module.exports = {
     hasMore: true,
     data: [
         {
-            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            img: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4095457842,4075848312&fm=27&gp=0.jpg',
             title: '汉堡大大',
             subTitle: '叫我汉堡大大，还你多彩口味',
             price: '28',
@@ -10,7 +10,7 @@ module.exports = {
             mumber: '389'
         },
         {
-            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            img: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2406941191,1951438913&fm=27&gp=0.jpg',
             title: '北京开源饭店',
             subTitle: '[望京]自助晚餐',
             price: '98',
@@ -18,7 +18,7 @@ module.exports = {
             mumber: '689'
         },
         {
-            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            img: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2406941191,1951438913&fm=27&gp=0.jpg',
             title: '服装定制',
             subTitle: '原价xx元，现价xx元，可修改一次',
             price: '1980',
@@ -26,7 +26,7 @@ module.exports = {
             mumber: '106'
         },
         {
-            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            img: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2406941191,1951438913&fm=27&gp=0.jpg',
             title: '婚纱摄影',
             subTitle: '免费试穿，拍照留念',
             price: '2899',
@@ -34,7 +34,7 @@ module.exports = {
             mumber: '58'
         },
         {
-            img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+            img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=541062957,1548234102&fm=27&gp=0.jpg',
             title: '麻辣串串烧',
             subTitle: '双人免费套餐等你抢购',
             price: '0',
