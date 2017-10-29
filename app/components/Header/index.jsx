62c867af44b16f48e959285ad4c9b81b1562694a
master <641471534@qq.com>
2017-10-29 16:40:34 +0800

    修改城市选择页面的顶部

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
+import '../../static/css/iconfont.css';
+import './style.less';
+
 class Header extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <h1>{this.props.title}</h1>
+    	<div className="header-component">
+        <span className= "header-icon" onClick={this.bandleFn.bind(this)}>
+          <i className="iconfont icon-huise common-back" ></i>
+        </span>
+	    	<h1>{this.props.title}</h1>
+	    </div>
     )
+  }
+  bandleFn(){
+  	window.history.back();
   }
 }
 
