8c29aa3441d92e0b626b8365e700d93cf392b826
master <641471534@qq.com>
2017-11-03 17:03:30 +0800

    搜索结果展示页面

---
+++ app/containers/Search/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import SearchHeader from '../../components/SearchHeader';
+import './style.less';
+
+class Search extends React.Component {
+  /* 如果要在组件结构中使用this.props 则要使用 super()继承 */
+  constructor(props) {
+    super(props);
+    this.state={
+    	value:'test input placahodler'
+    }
+  }
+  render() {
+    const keyword = this.props.params;
+    
+    return (
+    	<div>
+        <SearchHeader keyword={keyword.keyword}/>
+      </div>
+    )
+  }
+  clickHandle(){
+  	window.history.back();
+  }
+}
+
+export default Search;
