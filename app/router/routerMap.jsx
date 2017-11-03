7d9e8c73422aae80b11fde1903bbc3905a7af99e
master <641471534@qq.com>
2017-11-03 17:18:04 +0800

    添加路由

--- app/router/routerMap.jsx
+++ app/router/routerMap.jsx
@@ -3,6 +3,8 @@ import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import App from '../containers';
 import Home from '../containers/Home';
 import City from '../containers/city';
+import Search from '../containers/Search';
+import Details from '../containers/details'
 import NotFound from '../containers/404';
 
 export default class RouteMap extends React.Component {
@@ -16,6 +18,9 @@ export default class RouteMap extends React.Component {
 				<Route path="/" component={App}>
 					<IndexRoute component={Home}/>
           <Route path="/city" component={City}/>
+          <Route path="/search/:category(/:keyword)" component={Search}/>
+          <Route path="/details/:id" component={Details}/>
+          <Route path="*" component={NotFound}/>
 				</Route>
 			</Router>
     );
