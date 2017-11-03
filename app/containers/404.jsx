f35e46d7ed509a51cd238a8424d7c73a2b316182
master <641471534@qq.com>
2017-11-03 17:04:39 +0800

    404页面修改

--- app/containers/404.jsx
+++ app/containers/404.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 class NotFound extends React.Component {
   /* 如果要在组件结构中使用this.props 则要使用 super()继承 */
   constructor(props) {
     super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
   }
   render() {
     return (
