864e84c63d02cf7255324dc7434bbab1164a4b3f
master <641471534@qq.com>
2017-11-06 21:29:04 +0800

    搜索结果页，展示结果使用的是组件

--- app/containers/Search/index.jsx
+++ app/containers/Search/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import SearchHeader from '../../components/SearchHeader';
 import Likelist from '../../components/Likelist'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as userinfoActionsFormOtherFile from '../../actions/userinfo'
+import SearchList from './subpage/List'
 
 import './style.less';
 
@@ -8,22 +12,46 @@ class Search extends React.Component {
   /* 如果要在组件结构中使用this.props 则要使用 super()继承 */
   constructor(props) {
     super(props);
-    this.state={
-    	value:'test input placahodler'
-    }
   }
+
   render() {
-    const keyword = this.props.params;
+    const data = this.props;
+    const keyword = data.params.keyword;
+    const cityName = data.userinfo.cityName;
     
     return (
     	<div>
-        <SearchHeader keyword={keyword.keyword}/>
+        <SearchHeader keyword={keyword}/>
+
+        <SearchList keyword={keyword} cityName={cityName}/>
       </div>
     )
   }
-  clickHandle(){
-  	window.history.back();
-  }
+
 }
 
-export default Search;
+/**********   redux 绑定 react 中  在顶部需要引入  bindActionCreators  userinfoActionsFormOtherFile ****/
+// 将redux的值注入到 props 中，可以直接使用
+function mapStateToProps(state) {
+  return {
+    userinfo: state.userinfo
+  }
+}
+// 将 redux 的方法注入到 props 中，
+function mapDispatchToProps(dispatch){
+  return{
+    userinfoActions: bindActionCreators(userinfoActionsFormOtherFile, dispatch)
+  }
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search)
+/**
+ * 搜索页
+ * 1、抽离顶部组件
+ * 2、页面进入就加载首页数据
+ *     参数： 城市（redux）  页数（加载更多）
+ *     page, cityName, category, keyword     
+ * 3、处理加载更多，引用组件，从组件写父元素的事件
+ */
---
+++ app/containers/Search/subpage/List.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { getSearchData } from '../../../fetch/search/search';
+import Likelist from '../../../components/Likelist'
+import LoadMore from '../../../components/LoadMore'
+
+class SearchList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    this.state={
+    	 data : [],
+    	 page: 1,
+    	 hasMore: false,
+    	 isLoadingMore: false
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      	<Likelist data={this.state.data} />
+
+        <LoadMore isLoadMore={this.state.isLoadMore} LodaMoreData={this.LodaMoreData.bind(this)}/>
+      </div>
+    )
+  }
+
+  componentDidMount(){
+  	// 初次加载数据
+    this.firstpage();
+  }
+
+  firstpage(){
+    const data = getSearchData(this.props.cityName, this.props.keyword, 1, this.state.page);
+    this.disposeData(data);
+  }
+
+  LodaMoreData(){
+    this.setState({
+      isLoadMore: true
+    })
+    const page = this.state.page;
+
+    const data = getSearchData(this.props.cityName, this.props.keyword, 1, this.state.page);
+    this.disposeData(data)
+
+    this.setState({
+      page: page + 1,
+      isLoadMore: false
+    })
+  }
+
+  disposeData(result){
+
+  	result.then((res) => {
+  			return res.json();
+  	}).then((json) => {
+  			
+  			this.setState({
+  				data: this.state.data.concat(json.data),
+  				hasMore: json.hasMore
+  			})
+  	})
+  }
+
+  // 在页面从新搜索时
+  componentDidUpdate(prevProps, prevState) {
+      const keyword = this.props.keyword
+      console.log(prevProps);
+      console.log(keyword);
+
+      // 搜索条件完全相等时，忽略。重要！！！
+      if (keyword === prevProps.keyword) {
+          return
+      }
+
+      // 重置 state
+      this.setState({
+         data : [],
+         page: 1,
+         hasMore: false,
+         isLoadingMore: false
+      })
+
+      // 重新加载数据
+      this.firstpage();
+  }
+}
+
+export default SearchList;
+
+/**
+ * 初次加载数据
+ * 加载更多模块的事件函数展示
+ * /api/search/:cityName/:keyword/:id/:page
+ * 用户在本页面搜素，则需要考虑页面所有要处理的情况,不处理的话页面上存在的还时前一个搜索结果的内容
+ */
