755baeb09c111a66371cbe761c085452f2bc2b0f
master <641471534@qq.com>
2017-11-17 15:38:29 +0800

    登录组件的更新

--- app/components/Logincomponet/index.jsx
+++ app/components/Logincomponet/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
+import './style.less'
+
 class LoginComponent extends React.Component {
   /* 如果要在组件结构中使用this.props 则要使用 super()继承 */
   constructor(props) {
@@ -13,43 +15,56 @@ class LoginComponent extends React.Component {
 
   render() {
     return (
-      <h1>登陆组件</h1>
-    )
-  }
-  
-}
+      <div className="login-warp">
 
-export default LoginComponent;
-
-/*
-<div className="login-phone">
-              <span>
+          <div className="login-phone">
+              <span className="login-icon fl">
                   <i className="iconfont icon-jiantouxia"/>
               </span>
-              <input  type="text"
-                      className="login-phone-input"
-                      placeholder="输入手机号"
-                      value={this.state.value}
-                      onChange={this.changeHandle.bind(this)}/>
+              <div className="login-input-warp">
+                <input  type="text"
+                        className="login-phone-input"
+                        placeholder="输入手机号"
+                        value={this.state.username}
+                        onChange={this.changeHandle.bind(this)}/>
+              </div>
           </div>
 
           <div className="login-phone clearfix">
-              <span>
+              <span className="login-icon fl">
                   <i className="iconfont icon-jiantouxia"/>
               </span>
-              <input  type="text"
-                      className="login-phone-input"
-                      placeholder="输入验证码"
-                      value={this.state.verify}
-                      onChange={this.verifychangeHandle.bind(this)}/>
 
               <input  type="button"
                       className="login-verify-btn fr"
-                      value="获取验证码"
-                      onClick={this.clickHandle.bind(this)}/>
+                      value="获取验证码"/>
+
+              <div className="login-input-warp mr100">
+                <input  type="text"
+                        className="login-phone-input"
+                        placeholder="输入验证码"/>
+              </div>       
+              
           </div>
 
+
           <div className="login-btn-wrap">
-              <button className="login-btn" onClick={this.loginUp.bind(this)}>登陆</button>
+              <button className="login-btn" onClick={this.clickHandle.bind(this)}>登陆</button>
           </div>
- */
+      </div>
+    )
+  }
+  
+  changeHandle(e){
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  clickHandle(){
+    const params = this.props;
+    params.clickHandle(this.state.username);
+  }
+}
+
+export default LoginComponent;
