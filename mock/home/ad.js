30ee3210d6c45d289f8ac1e223c869a7fd53ad15
wjnote <641471534@qq.com>
2018-07-23 14:10:33 +0800

    .....

--- mock/home/ad.js
+++ mock/home/ad.js
@@ -1,32 +1,25 @@
-module.exports = [
-    {
-        title: '暑假5折',
-        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1509003006&di=5f21577bc2a6dd650174eae640ed3f3d&imgtype=jpg&er=1&src=http%3A%2F%2Fimage.tianjimedia.com%2FuploadImages%2F2012%2F012%2F50HJ0RDE8IM2.jpg',
-        link: 'http://www.baidu.com/'
-    },
-    {
-        title: '特价出国',
-        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508408306492&di=3d50ea16afc9c15ae7528cf320016a9f&imgtype=jpg&src=http%3A%2F%2Ft1.du114.com%2Fuploads%2Ftu%2F20150716%2F4ptheqri5gk.jpg',
-        link: 'http://www.baidu.com/'
-    },
-    {
-        title: '亮亮车',
-        img: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4095457842,4075848312&fm=27&gp=0.jpg',
-        link: 'http://www.baidu.com/'
-    },
-    {
-        title: '学钢琴',
-        img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=80419802,834981519&fm=27&gp=0.jpg',
-        link: 'http://www.baidu.com/'
-    },
-    {
-        title: '电影',
-        img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
-        link: 'http://www.baidu.com/'
-    },
-    {
-        title: '旅游热线',
-        img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1998443773,2783746098&fm=27&gp=0.jpg',
-        link: 'http://www.baidu.com/'
-    }
-]
+module.exports = [{
+  title: '暑假5折',
+  img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1509003006&di=5f21577bc2a6dd650174eae640ed3f3d&imgtype=jpg&er=1&src=http%3A%2F%2Fimage.tianjimedia.com%2FuploadImages%2F2012%2F012%2F50HJ0RDE8IM2.jpg',
+  link: 'http://www.baidu.com/'
+}, {
+  title: '特价出国',
+  img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1998443773,2783746098&fm=27&gp=0.jpg',
+  link: 'http://www.baidu.com/'
+}, {
+  title: '亮亮车',
+  img: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4095457842,4075848312&fm=27&gp=0.jpg',
+  link: 'http://www.baidu.com/'
+}, {
+  title: '学钢琴',
+  img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=80419802,834981519&fm=27&gp=0.jpg',
+  link: 'http://www.baidu.com/'
+}, {
+  title: '电影',
+  img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1095273209,813028068&fm=27&gp=0.jpg',
+  link: 'http://www.baidu.com/'
+}, {
+  title: '旅游热线',
+  img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1998443773,2783746098&fm=27&gp=0.jpg',
+  link: 'http://www.baidu.com/'
+}]
